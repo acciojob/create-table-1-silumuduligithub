0d0af72660834497164351f95fe379403eeafe6c
Silu Muduli <130176819+silumuduligithub@users.noreply.github.com>
2023-10-02 16:36:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  let table = document.getElementById("sampleTable");
+	let childrens = table.children[0];
+	console.log(childrens);
+	let tr = document.createElement("tr");
+	for(let i = 0; i < 2; i++){
+		let td = document.createElement("td");
+		if(i == 0){
+			td.innerText = "New Cell1";
+		}else{
+			td.innerText = "New cell2";
+		}
+		tr.appendChild(td);
+	}
+	table.insertBefore(tr, childrens);	
 }
